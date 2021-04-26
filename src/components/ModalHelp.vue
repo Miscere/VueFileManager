a5ee8b1cfa49ee581628a38c92b7dd1ce3844c91
Iago <iago.borba.almeida@gmail.com>
2021-04-26 19:22:36 -0300

    Memory controlling update

---
+++ src/components/ModalHelp.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="modal fade" id="modalHelp" tabindex="-1" aria-labelledby="modalHelpLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalHelpLabel">
+                        <i class="fa fa-question-circle">&nbsp;</i>
+                        Help
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-start">
+                    <ul class="list-group">
+                        <li class="list-group-item" v-for="(command, commandName) in commands" :key="'valid_command_'+commandName">
+                            <h6 data-bs-toggle="collapse" :href="'#commands_help_collapse_'+commandName">> {{commandName}}</h6>
+                            <div class="collapse pb-2" :id="'commands_help_collapse_'+commandName">
+                                <span class="text-muted" v-html="command.description"></span>
+                            </div>
+                    
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ModalHelp',
+
+    props: {
+        commands: Object
+    }
+}
+</script>
